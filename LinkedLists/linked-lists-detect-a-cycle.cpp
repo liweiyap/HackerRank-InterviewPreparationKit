1349c4696e39aaf56d643fb102ce47c0d3d2cd22
liweiyap <liweiyap@gmail.com>
2019-11-19 18:28:14 +0100

    Add code for Linked Lists: Detect a Cycle

---
+++ LinkedLists/linked-lists-detect-a-cycle.cpp
@@ -0,0 +1,23 @@
+struct Node;
+
+// IN: head of linked list
+// OUT: boolean for whether linked list has cycle
+bool has_cycle(Node* head) {
+    // Floyd's cycle-finding algorithm: move slow walker by one and fast walker by two
+    // cycle exists if both walkers ever meet
+    Node* slow_walker = head;
+    Node* fast_walker = head;
+    while (slow_walker && fast_walker && fast_walker->next){
+        slow_walker = slow_walker->next;
+        fast_walker = fast_walker->next->next;
+        if (slow_walker == fast_walker){
+            return true;
+        }
+    }
+    return false;
+}
+
+struct Node{
+    int data;
+    struct Node* next;
+}
